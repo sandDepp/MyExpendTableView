b696a419ac7b89c67344e157b7a9284e0c2508a2
Sand <sandhungry@126.com>
2015-12-08 10:18:23 +0800

    Initial Commit

---
+++ MyExpendTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyExpendTableView
+//
+//  Created by 沙莎 on 15/12/8.
+//  Copyright © 2015年 沙莎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MyExpendTableView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MyExpendTableView
+//
+//  Created by 沙莎 on 15/12/8.
+//  Copyright © 2015年 沙莎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
